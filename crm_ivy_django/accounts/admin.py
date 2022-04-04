235c42f4ff28c157587b8b52b0576a112d60d979
don <stivnnkolov@gmail.com>
2022-04-04 18:42:00 +0300

    adding models Customer, Product, Order. Register the models in the admin

--- crm_ivy_django/accounts/admin.py
+++ crm_ivy_django/accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from crm_ivy_django.accounts.models import Customer, Product, Order
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
--- crm_ivy_django/accounts/models.py
+++ crm_ivy_django/accounts/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    NAME_MAX_LEN = 200
+    PHONE_MAX_LEN = 200
+
+    name = models.CharField(
+        max_length=NAME_MAX_LEN,
+        null=True,
+        blank=True,
+    )
+    phone = models.CharField(
+        max_length=PHONE_MAX_LEN,
+        null=True,
+        blank=True,
+    )
+    email = models.EmailField(
+        null=True,
+        blank=True,
+    )
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    NAME_MAX_LEN = 200
+    CATEGORY_CHOICES = ['Indoor', 'Out Door']
+
+    name = models.CharField(
+        max_length=NAME_MAX_LEN,
+        null=True,
+        blank=True,
+    )
+    price = models.FloatField(
+        null=True,
+        blank=True,
+    )
+    category = models.CharField(
+        max_length=max(len(el) for el in CATEGORY_CHOICES),
+
+        choices=((el, el) for el in CATEGORY_CHOICES),
+    )
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    ORDER_CHOICES = ['Pending', 'Out for delivery', 'Delivered']
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+    )
+    status = models.CharField(
+        max_length=max(len(el) for el in ORDER_CHOICES),
+        choices=((el, el) for el in ORDER_CHOICES),
+        null=True,
+        blank=True,
+    )
+    # products =
+    # customer =
