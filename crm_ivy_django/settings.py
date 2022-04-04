717756bf2850631588447da01355d30b767d306b
don <stivnnkolov@gmail.com>
2022-04-04 17:50:33 +0300

    setting static files and adding logo

--- crm_ivy_django/settings.py
+++ crm_ivy_django/settings.py
@@ -81,5 +81,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = 'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
